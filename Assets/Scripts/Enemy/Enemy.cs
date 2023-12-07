65a1d776fa38e7089bcd740f9fa5f98034f9d566
Ferdlcz <ferdlcskate@gmail.com>
2023-12-06 21:18:56 -0600

    NavMesh en enemigos

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
@@ -9,7 +10,9 @@ public class Enemy : MonoBehaviour
     public GameObject target;
     public EnemyRange range;
 
-
+    public NavMeshAgent agent;
+    public float attackDistance;
+    public float radioVision;
     public bool attack;
     public bool stunned;
     public float routine;
@@ -29,7 +32,7 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         anim = GetComponent<Animator>();
-
+        agent = GetComponent<NavMeshAgent>();
         target = GameObject.Find("PrincipalCharacter");
     }
 
@@ -48,20 +51,21 @@ public class Enemy : MonoBehaviour
         Behavior_Enemy();
     }
 
-    void Behavior_Enemy()
+   void Behavior_Enemy()
     {
-        if (Vector3.Distance(transform.position, target.transform.position) > 20)
+        if (Vector3.Distance(transform.position, target.transform.position) > radioVision)
         {
-
+            agent.isStopped = true;
             seeYou.Play();
             anim.SetBool("run", false);
-            chronometer += 1 * Time.deltaTime;
+            chronometer += Time.deltaTime;
 
             if (chronometer >= 4)
             {
                 routine = Random.Range(0, 2);
                 chronometer = 0;
             }
+
             switch (routine)
             {
                 case 0:
@@ -75,43 +79,48 @@ public class Enemy : MonoBehaviour
                     break;
 
                 case 2:
-                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angle, 0.5f);
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angle, 180.0f * Time.deltaTime);
                     transform.Translate(Vector3.forward * 1 * Time.deltaTime);
                     anim.SetBool("walk", true);
+                    break;
+
+                case 3:
+                    routine = 0;
                     break;
             }
         }
         else
         {
+            agent.isStopped = false;
+            agent.SetDestination(target.transform.position);
 
-                var lookPos = target.transform.position - transform.position;
-                lookPos.y = 0;
-                var rotation = Quaternion.LookRotation(lookPos);
-
-            if (Vector3.Distance(transform.position, target.transform.position) > 1f && !attack)
+            if (Vector3.Distance(transform.position, target.transform.position) > attackDistance && !attack)
             {
-
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
                 anim.SetBool("walk", false);
                 anim.SetBool("run", true);
-                transform.Translate(Vector3.forward * 5 * Time.deltaTime);
-
-                anim.SetBool("attack", false);
             }
             else
             {
-                if(!stunned && !attack){
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);   
-                anim.SetBool("walk", false);
-                anim.SetBool("run", false);
+                if (!attack)
+                {
+                    anim.SetBool("walk", true);
+                    anim.SetBool("run", false);
                 }
-                
             }
+        }
+
+        if (attack)
+        {
+            agent.isStopped = true;
         }
     }
 
-    public void Stop_Attack(){
-        anim.SetBool("attack", false);
+    public void Stop_Attack()
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) > attackDistance + 0.2f)
+        {
+            anim.SetBool("attack", false);
+        }
         attack = false;
         stunned = false;
         range.GetComponent<CapsuleCollider>().enabled = true;
